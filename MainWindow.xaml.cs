59ed667a35cd9f73cc3415ee33a6784b29b4aea1
qeuete <mkkkk_21@mail.ru>
2024-03-16 14:51:40 +0300

    aaaaaaaaaaaa

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ClinetsTable(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new ClientsWindow();
+        }
+        private void RoomsTable(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new RoomsWindow();
+        }
+        private void BookingTable(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new BookingWindow();
+        }
+    }
+}
